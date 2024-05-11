ea63458543ce03ced904c62dba9a83b29ab44228
alfytka <lyzexx04@gmail.com>
2024-05-11 20:19:32 +0700

    redux - state management

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -6,6 +6,8 @@ import LoginPage from './pages/login.jsx'
 import RegisterPage from './pages/register.jsx'
 import ErrorPage from './pages/404.jsx'
 import ProductsPage from './pages/products.jsx'
+import ProfilePage from './pages/profile.jsx'
+import DetailProductPage from './pages/detailProduct.jsx'
 
 const router = createBrowserRouter([
   {
@@ -24,7 +26,15 @@ const router = createBrowserRouter([
   {
     path: "/products",
     element: <ProductsPage />,
-  }
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
+  {
+    path: "/product/:id",
+    element: <DetailProductPage />,
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
